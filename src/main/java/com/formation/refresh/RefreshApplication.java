a92b15498581423298a81237c95cc54cb3ffca70
zeineb <zeineb.guellali@gmail.com>
2023-12-11 18:35:13 +0100

    feat : HELLO WORLD

--- src/main/java/com/formation/refresh/RefreshApplication.java
+++ src/main/java/com/formation/refresh/RefreshApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RefreshApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RefreshApplication.class, args);
+	public static void main(String[] args) {SpringApplication.run(RefreshApplication.class, args);
 	}
 
 }
